5c23dd94765f8b63451d2afec358fd61c14bd11f
wearesober <thesobrietycollective@gmail.com>
2016-05-17 19:56:26 -0400

    YAY!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-
+	event.preventDefault();
+	
  $('[data-toggle="popover"]').popover(); 
-
 });
+
+//When user submits form, have alert pop up. 
+
+  $("form").submit(function(){
+  	event.preventDefault();
+    alert("Thanks for Playing!");
+ });
